8de2aee92a2db48b56e6c9128422f5b7b02d4ce6
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-05-22 23:49:17 +0300

    list_init

--- src/Language.ml
+++ src/Language.ml
@@ -34,8 +34,10 @@ module Value =
     | Sexp (t, _) -> t
     | _ -> failwith "symbolic expression expected"
 
+    let rec list_init i n f = if i >= n then [] else (f i) :: (list_init (i + 1) n f)
+
     let update_string s i x = String.init (String.length s) (fun j -> if j = i then x else s.[j])
-    let update_array  a i x = List.init   (List.length a)   (fun j -> if j = i then x else List.nth a j)
+    let update_array  a i x = list_init 0 (List.length a)   (fun j -> if j = i then x else List.nth a j)
 
   end
        
